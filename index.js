3b3cf183e2f6e4a7f611b2639961d0f87c47c9e2
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-04-28 12:04:36 +0100

    Adds new styles

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const countDownDate = new Date("May 18, 2022 12:00:00").getTime();
+    // const countDownDate = new Date("April 28, 2022 12:00:00").getTime();
+
+    const countdown = setInterval(() => {
+    
+      const now = new Date().getTime();
+    
+      const timeLeft = countDownDate - now;
+    
+      const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+      document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    
+      if (timeLeft < 0) {
+        clearInterval(countdown);
+        document.getElementById("countdown").innerHTML = "LAUNCH!!!";
+        document.getElementById("pyro").style.display = "block";
+      }
+
+    }, 1000);
+});
