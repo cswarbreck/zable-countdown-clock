8dc2fa4a38d03fb588dcdecb21b3ccc6a526a532
Cosmo Swarbreck <c.swarbreck@gmail.com>
2022-05-12 11:57:28 +0100

    Amends date and styles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const countDownDate = new Date("May 18, 2022 12:00:00").getTime();
+    const countDownDate = new Date("May 25, 2022 12:00:00").getTime();
     // const countDownDate = new Date("April 28, 2022 12:00:00").getTime();
 
     const countdown = setInterval(() => {
